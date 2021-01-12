3d79e329adfb0c396a2a9426ec7418f1d4dfee44
elaaarfaoui <elaa.arfaoui1@gmail.com>
2021-01-12 15:27:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import logogomycode from './logogomycode.png';
+import { Card } from "react-bootstrap";
 import './App.css';
+import ReactPlayer from 'react-player'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+<div className="title"> 
+      <img className="image1" src={logogomycode} alt="Logo" />
+      <h1 className="titlered " >Programme de  bourses Go My Code</h1>
+</div>
+<div className="cartes">
+<Card className="carte" style={{ width: '18rem' }}>
+  <Card.Img style={{width:"100%"}} variant="top" src= "./img1.png" />
+  <Card.Body>
+    <Card.Title style={{ textAlign:"center", fontSize: "20px", fontWeight:"bold"}}>Programme de bourses pour les femmes</Card.Title>
+    <Card.Text>
+    Le programme de bourses ELLEVATE de GOMYCODE vise à introduire plus de femmes tunisiennes dans le monde de la technologie, à les préparer à l'avenir et à leur donner un petit coup de pouce pour faciliter leur intégration dans le monde du travail.
+    </Card.Text>
+  </Card.Body>
+</Card>
+
+<Card className="carte" style={{ width: '18rem' }}>
+  <Card.Img style={{width:"100%"}} variant="top" src= "./talan.jpg" />
+  <Card.Body>
+    <Card.Title style={{ textAlign:"center", fontSize: "20px", fontWeight:"bold"}}>Programme de bourses pour 1000 développeurs</Card.Title>
+    <Card.Text>
+    Ce programme vise à former les jeunes diplômés de l’enseignement supérieur tunisien sur le développement web 
+en vue de faciliter leur intégration professionnelle.Ce programme s’adresse aux jeunes diplômés de l’enseignement supérieur qui veulent démarrer une carrière dans le numérique.
+    </Card.Text>
+  </Card.Body>
+</Card>
+
+<Card className="carte" style={{ width: '18rem' }}>
+  <Card.Img style={{width:"100%"}} variant="top" src= "./img3.jpg" />
+  <Card.Body>
+    <Card.Title style={{ textAlign:"center", fontSize: "20px", fontWeight:"bold"}}>Programme de  bourses « Algerian Women in Tech »</Card.Title>
+    <Card.Text>
+    Gomycode lance « Algerian Women in Tech », la première bourse pour offrir aux femmes l’opportunité de s’approprier le futur grâce à l’accès à la technologie et forme déjà plusieurs centaines d’étudiants au développement informatique et aux nouvelles technologies comme l’intelligence artificielle et la data science à travers tout le pays.
+    </Card.Text>
+  </Card.Body>
+</Card>
+</div>
+
+<ReactPlayer className="sequence" url='https://youtu.be/olJOZp57FWM' />
+</div>
   );
 }
 
